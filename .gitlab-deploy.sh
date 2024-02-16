a5877a6486365f246c55ee08accd48001ad8c9ac
Slim Zegarra <slim.zegarra@panbox.io>
2024-02-16 12:52:19 -0500

    Modificando comando scrip deploy

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -6,5 +6,5 @@ array=(${string//,/ })
 #Iterate servers for deploy and pull last commit
 for i in "${!array[@]}"; do
       echo "Deploy project on server ${array[i]}"
-      ssh ubuntu@${array[i]} "sudo su && cd fileupload  && sudo git pull "https://PySlim:"$SSH_PRIVATE_PASS"@gitlab.com/PySlim/fileupload.git"  main && sudo docker-compose -f develop.yml down && sudo docker-compose -f develop.yml up -d"
+      ssh ubuntu@${array[i]} "sudo su && cd fileupload  && sudo git pull "https://PySlim:"$SSH_PRIVATE_PASS"@gitlab.com/PySlim/fileupload.git"  main && sudo docker compose -f develop.yml down && sudo docker compose -f develop.yml up -d"
 done
