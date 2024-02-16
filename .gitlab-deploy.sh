9e0450393e5082a68138322ee984387a8ec59aa2
Slim Zegarra <slim.zegarra@panbox.io>
2024-02-16 12:48:02 -0500

    Implementando cd/ci

---
+++ .gitlab-deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Get servers list
+set -f
+string=$DEPLOY_SERVER
+array=(${string//,/ })
+#Iterate servers for deploy and pull last commit
+for i in "${!array[@]}"; do
+      echo "Deploy project on server ${array[i]}"
+      ssh ubuntu@${array[i]} "sudo su && cd fileupload  && sudo git pull "https://PySlim:"$SSH_PRIVATE_PASS"@gitlab.com/PySlim/fileupload.git"  main && sudo docker-compose -f develop.yml down && sudo docker-compose -f develop.yml up -d"
+done
